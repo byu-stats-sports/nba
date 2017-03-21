587866b7338f41d2becc0c8184fe14e96f6e6872
Christopher Beckett <chrisbeckett8@gmail.com>
2017-03-21 17:05:33 -0600

    Commit Message

--- nba/__init__.py
+++ nba/__init__.py
@@ -7,6 +7,7 @@ __author__ = 'William Myers'
 __version__ = '0.0.1'
 __licence__ = 'GPLv3+'
 
-DATABASE_URL = os.environ['BYU_NBA_DATABASE_URL']
+#Comment this database_url line out when running code. 
+#DATABASE_URL = os.environ['BYU_NBA_DATABASE_URL']
 #  CURRENT_SEASON = nba_py.constants.CURRENT_SEASON
 CURRENT_SEASON = '2015-16'
--- nba/downloader.py
+++ nba/downloader.py
@@ -5,6 +5,8 @@ import nba_py.league
 import nba_py.draftcombine
 import dateutil.parser
 import datetime
+import sys
+sys.path.append('/Users/chris/Desktop/NBA Injury Data/nba')
 import nba
 import nba.utils
 import logging
@@ -15,12 +17,11 @@ logger = logging.getLogger(__name__)
 def fetch_players(season=None, only_current=1):
     logger.info('Downloading player data...')
     if not season:
-        season = nba.utils.valid_season(nba.CURRENT_SEASON)
-        # if the user does not specify a season, download all seasons
+        season = nba.utils.valid_season(nba.CURRENT_SEASON)# if the user does not specify a season, download all seasons
         only_current = 0
-
-    draft_combine = {}
-    # TODO: handle only_current season...
+    else:
+        season = nba.utils.valid_season(season)
+    draft_combine = {}# TODO: handle only_current season...
     for p in nba_py.draftcombine.DrillResults(season=season.raw).overall():
         player = {
             'bench_press': p['BENCH_PRESS'],
@@ -29,20 +30,15 @@ def fetch_players(season=None, only_current=1):
             'modified_lane_agility_time': p['MODIFIED_LANE_AGILITY_TIME'],
             'standing_vertical_leap': p['STANDING_VERTICAL_LEAP']
         }
-        draft_combine[p['PLAYER_NAME']] = player
-
-    # use dictionary to ensure uniqueness
+        draft_combine[p['PLAYER_NAME']] = player# use dictionary to ensure uniqueness
     players = {}
     for p in nba_py.player.PlayerList(season=season.raw,
                                       only_current=only_current).info():
         if p['GAMES_PLAYED_FLAG'] is 'N':
             continue
-
         player = nba_py.player.PlayerSummary(p['PERSON_ID']).info()[0]
-
         if p['ROSTERSTATUS'] is 'Inactive':
             continue
-
         player_id = player['PERSON_ID']
         first_name = player['FIRST_NAME']
         last_name = player['LAST_NAME']
@@ -57,46 +53,36 @@ def fetch_players(season=None, only_current=1):
             'to_year': nba.utils.season_end(player['TO_YEAR']),
             'position': player['POSITION'] or None,
         }
-        
-        try:
-            # TODO: is there any better way of id than first and last name?
+        try:# TODO: is there any better way of id than first and last name?
             player.update(draft_combine[p['DISPLAY_FIRST_LAST']])
         except:
             pass
-        
         players[player_id] = player
-
     return players.values()
 
 
 def fetch_teams():
-    logger.info('Downloading team data...')
-
-    # use dictionary to ensure uniqueness
+    logger.info('Downloading team data...') # use dictionary to ensure uniqueness
     teams = {}
     for t in nba_py.team.TeamList().info():
         try:
             team = nba_py.team.TeamDetails(t['TEAM_ID']).background()[0]
             del(team['YEARFOUNDED'])
-        except IndexError:
-            #  historical team
+        except IndexError:#  historical team
             continue
-
         team['MIN_YEAR'] = datetime.datetime(int(t['MIN_YEAR']), 1, 1)
         team['MAX_YEAR'] = datetime.datetime(int(t['MAX_YEAR']), 1, 1)
         team = dict((k.lower(), v) for k, v in team.items())
         teams[team['team_id']] = team
-
     return teams.values()
 
 
 def fetch_team_rosters(season=None):
     logger.info('Downloading team rosters data...')
-
     if not season:
         season = nba.utils.valid_season(nba.CURRENT_SEASON)
-
-    # use dictionary to ensure uniqueness
+    else:
+        season = nba.utils.valid_season(season)# use dictionary to ensure uniqueness
     players = {}
     for t in nba_py.team.TeamList().info():
         for p in nba_py.team.TeamCommonRoster(season=season.raw,
@@ -108,39 +94,31 @@ def fetch_team_rosters(season=None):
                 'season_end': season.end
             }
             players[player['player']] = player
-
     return players.values()
 
 
 def fetch_games(season=None):
-    logger.info('Downloading game data...')
-    
-    # TODO: add points for each team?
+    logger.info('Downloading game data...')# TODO: add points for each team?
     if not season:
         season = nba.utils.valid_season(nba.CURRENT_SEASON)
-
+    else:
+        season = nba.utils.valid_season(season)
     winners = {}
     losers = {}
     games = []
     inactive_players = []
-
     for g in nba_py.league.GameLog(season=season.raw,
                                    counter=10000000,
                                    player_or_team='T').overall():
         d = winners if g['WL'] is 'W' else losers
-        d[g['GAME_ID']] = g['TEAM_ID']
-
-    # all matchups in season (1230 = 15 teams * 82 games)
+        d[g['GAME_ID']] = g['TEAM_ID'] # all matchups in season (1230 = 15 teams * 82 games)
     for game_id in winners.keys():
         game = nba_py.game.BoxscoreSummary(game_id)
-
         for p in game.inactive_players():
             player = {'game': int(game_id), 'player': p['PLAYER_ID']}
             inactive_players.append(player)
-
         game_info = game.game_info()[0]
         game_summary = game.game_summary()[0]
-
         game = {
             'game_id': game_id,
             'season_start': season.start,
@@ -155,16 +133,15 @@ def fetch_games(season=None):
             'loser_team': losers[game_id]
         }
         games.append(game)
-
     return (games, inactive_players)
 
 
 def fetch_games_by_player(season=None):
     logger.info('Downloading game by player data...')
-    
     if not season:
         season = nba.utils.valid_season(nba.CURRENT_SEASON)
- 
+    else:
+        season = nba.utils.valid_season(season)        
     games = []
     for g in nba_py.league.GameLog(season=season.raw,
                                    counter=10000000,
@@ -194,5 +171,4 @@ def fetch_games_by_player(season=None):
             'three_point_field_goals_percentage': g['FG3_PCT']
         }
         games.append(game)
-
     return games
