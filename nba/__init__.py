5939a2e4510ba51482bf45866b8ca565bdf3d911
chrisbeckett8 <chrisbeckett8@gmail.com>
2017-04-20 12:57:05 -0600

    Update __init__.py

--- nba/__init__.py
+++ nba/__init__.py
@@ -1,6 +1,5 @@
-"""
-nba - a CLI to manage BYU Sports Statistics NBA data.
-"""
+
+#nba - a CLI to manage BYU Sports Statistics NBA data.
 import os
 import nba_py
 import nba
@@ -9,5 +8,5 @@ __version__ = '0.0.1'
 __licence__ = 'GPLv3+'
 
 #Comment this database_url line out when running code. 
-DATABASE_URL = os.environ['BYU_NBA_DATABASE_URL']
-#CURRENT_SEASON = nba_py.constants.CURRENT_SEASON
+#DATABASE_URL = os.environ['BYU_NBA_DATABASE_URL']
+CURRENT_SEASON = nba_py.constants.CURRENT_SEASON
