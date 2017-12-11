0e4e8255ae798ab1f20d26045de072d5a96ca938
nate-d-andrews94 <nate.d.andrews94@gmail.com>
2017-12-11 13:46:40 -0700

    Update __init__.py

--- nba/__init__.py
+++ nba/__init__.py
@@ -8,5 +8,5 @@ __version__ = '0.0.1'
 __licence__ = 'GPLv3+'
 
 #Comment this database_url line out when running code. 
-DATABASE_URL = os.environ['BYU_NBA_DATABASE_URL']
+DATABASE_URL = os.getenv('BYU_NBA_DATABASE_URL')
 CURRENT_SEASON = nba_py.constants.CURRENT_SEASON
